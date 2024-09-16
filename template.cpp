68263831068aba545c3a0d4757b043a929349162
ILHAM LIGAR SAMUDRA <126567661+Ilham12359@users.noreply.github.com>
2024-09-16 11:00:22 +0700

    template

---
+++ template.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long;
+#define endl '\n';
+#define MOD 1e9+7;
+#define run freopen("input.txt","r",stdin);freopen("output.txt","w",stdout);
+#define bismillah ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+
+int main(){
+  bismillah
+  run
+
+ return 0;
+}
